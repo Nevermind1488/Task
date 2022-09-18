9c67eac47a2ad83fa9dc527d1e88e86ce19ab149
Nevermind1488 <2nextlvlplay2@gmail.com>
2022-09-18 23:55:56 +0300

    initial commit

--- src/Task2.java
+++ src/Task2.java
@@ -1,29 +1,30 @@
+import java.util.Random;
 import java.util.Arrays;
 public class Task2 {
     public static void main(String[] args) {
-        getArithmeticProgression(getRandom(),getRandom(), getRandom());
+        Random rand = new Random();
+        int[] arr = new int[3];
+        arr[0] = rand.nextInt(11);
+        arr[1] = rand.nextInt(11);
+        arr[2] = rand.nextInt(11);
+        Arrays.sort(arr);
+        getArithmeticProgression(arr);
+        getGeometricProgression(arr);
     }
-
-    public static int getRandom() {
-        int a = 0;
-        int b = 10;
-        int x =(int)(Math.random() * ((b - a) + 1));
-        return x;
-    }
-    int a = getRandom();
-    int b = getRandom();
-    int c = getRandom();
-
-    public static void getArithmeticProgression(int a, int b, int c) {
-           int [] arr = new int[]{a, b, c};
-           Arrays.sort(arr);
+    public static void getArithmeticProgression(int[] arr) {
            if(arr[1] - arr[0] == arr[2] - arr[1] && arr[2] != arr[1] && arr[0] != arr[1]){
-               System.out.printf("Можно составить арифметическую прогрессию:\n%d\n%d\n%d",a,b,c);
+               System.out.printf("Можно составить арифметическую прогрессию:\n%d\n%d\n%d\n",arr[0], arr[1], arr[2]);
            }
            else{
-               System.out.printf("Арифметическую прогрессию составить невозможно:\n%d\n%d\n%d",a,b,c);
+               System.out.printf("Арифметическую прогрессию составить невозможно:\n%d\n%d\n%d\n", arr[0], arr[1], arr[2]);
            }
-           return;
+    }
+    public static void getGeometricProgression(int[] arr) {
+        if(arr[0] != 0 && arr[1] != 0 && Math.abs(((double) arr[1] / arr[0]) - ((double) arr[2] / arr[1])) < 1e-3){
+            System.out.printf("Можно составить геометрическую прогрессию:\n%d\n%d\n%d",arr[0], arr[1], arr[2]);
+        }
+        else{
+            System.out.printf("Геометрическую прогрессию составить невозможно:\n%d\n%d\n%d", arr[0], arr[1], arr[2]);
+        }
     }
 }
-
